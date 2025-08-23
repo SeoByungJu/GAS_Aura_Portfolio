70b2abc0b298e7a2ce0520ca86b31a96080e30c5
Seobyungju <qudwn0928@gmail.com>
2025-08-24 01:43:39 +0900

    feat(attributes): 섹션 8 - RPG Attributes 구현 완료
    - DataTable 기반 초기 Attribute 세팅 (65강)
    - GameplayEffect로 Attribute 초기화 구조 추가 (66강)
    - Attribute 기반 Modifier 및 연산 순서 적용 (67~68강)
    - Modifier Coefficient 적용 로직 설계 (69강)
    - Secondary/Derived Attributes 계산 구조 구현 (70~71강)
    - Custom Calculation 클래스 적용 (72강)
    - Player Level 및 Combat Interface 연동 (73강)
    - Modifier Magnitude 계산 흐름 완성 (74강)
    - Vital Attributes 초기화 로직 정리 (75강)
    
    🎯 핵심: Attribute 초기화 → Modifier 적용 → 파생/2차 스탯 계산 → Vital/Level 연계까지
    RPG 시스템의 전투 기반 Attribute 구조 전면 구현 완료

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -16,7 +16,12 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 public:
 	AAuraCharacter();
 	virtual void PossessedBy(AController* NewController) override;
-	virtual void OnRep_PlayerState() override;;
+	virtual void OnRep_PlayerState() override;
+
+	/** Combat Interface*/
+	virtual int32 GetPlayerLevel() override;
+	/** End Combat Interface*/
+	
 private:
 	virtual void InitAbilityActorInfo() override;
 };
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
-#include "Engine/RendererSettings.h"
 #include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
@@ -17,10 +16,21 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
+	/** Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	/** End Enemy Interface */
+
+	/** Combat Interface */
+	virtual int32 GetPlayerLevel() override;
+
+	/** End Combat Interface */
 	
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
+	
 };
