bd9cd808f9c852fee0c3440655f63a1a38a38363
Seobyungju <qudwn0928@gmail.com>
2025-09-28 11:17:01 +0900

    feat(section21): 242강 - SpellGlobe Delegate 바인딩 및 클라이언트 동기화
    - SpellGlobe 위젯에 InputTag 변수 추가 및 AbilityInfoDelegate 바인딩 처리
    - ReceiveAbilityInfo() 구현 → AbilityInfo 수신 후 아이콘/배경 이미지 적용
    - WBP_HealthManaSpells에 Spell Globe 변수화 및 InputTag 사전 지정 (LMB, RMB, 1~4, Passive1~2)
    - SetSpellGlobeInputTags()로 초기화, PreConstruct 단계에서 실행
    - SetGlobeWidgetControllers() 함수로 SpellGlobes에 WidgetController 할당
    - AuraAbilitySystemComponent에 OnRep_ActivateAbilities() 오버라이드
    - bStartupAbilitiesGiven 검사 후 최초 1회 AbilitiesGivenDelegate 클라이언트 Broadcast
    - HUD에 멀티플레이어 클라이언트 동기화 구조 완성

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -90,32 +90,32 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	 */
 	
 	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
-		FName("Input.LMB"),
+		FName("InputTag.LMB"),
 		FString("Input Tag for Left Mouse Button")
 		);
 
 	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Input.RMB"),
+	FName("InputTag.RMB"),
 	FString("Input Tag for Right Mouse Button")
 	);
 	
 	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Input.1"),
+	FName("InputTag.1"),
 	FString("Input Tag for 1 Key")
 	);
 
 	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Input.2"),
+	FName("InputTag.2"),
 	FString("Input Tag for 2 Key")
 	);
 
 	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Input.3"),
+	FName("InputTag.3"),
 	FString("Input Tag for 3 Key")
 	);
 
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
-	FName("Input.4"),
+	FName("InputTag.4"),
 	FString("Input Tag for 4 Key")
 	);
 
