13678db68bb9a23e98ed1e8c66ba24db615aa84f
Seobyungju <qudwn0928@gmail.com>
2025-09-29 22:27:29 +0900

    feat(section22): 262강 - 레벨업 VFX 멀티캐스트 구현
    - AuraCharacter에 LevelUpNiagaraComponent 멤버 추가 및 RootComponent에 Attach
    - NiagaraComponent bAutoActivate = false 설정
    - LevelUp_Implementation()에서 Multicast_LevelUpParticles() 호출 추가
    - Multicast_LevelUpParticles() 함수 구현 (NetMulticast, Reliable)
      · NiagaraComponent->Activate(true) 호출
      · Camera 위치/회전값을 참조하여 Niagara 이펙트 카메라 정렬 처리
    - CameraBoom 및 TopDownCameraComponent를 C++로 이전
      · CameraBoom: TargetArmLength = 800, AbsoluteRotation = true, bDoCollisionTest = false
      · TopDownCameraComponent: SpringArm 끝단에 Attach, bUsePawnControlRotation = false
    - AuraCharacter BP에서 LevelUpNiagaraComponent의 NiagaraSystem을 NS_LevelUp으로 세팅
    - 레벨업 시 멀티플레이 환경에서도 일관된 시각 효과 보장

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,15 +3,32 @@
 
 #include "Character/AuraCharacter.h"
 #include "AbilitySystemComponent.h"
+#include "NiagaraComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/Data/LevelUpInfo.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Player/AuraPlayerController.h"
 #include "Player/AuraPlayerState.h"
 #include "UI/HUD/AuraHUD.h"
 
 AAuraCharacter::AAuraCharacter()
 {
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->SetUsingAbsoluteRotation(true);
+	CameraBoom->bDoCollisionTest = false;
+	CameraBoom->bEnableCameraLag = true;
+
+	TopdownCameraComponent = CreateDefaultSubobject<UCameraComponent>("TopdownCameraComponent");
+	TopdownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	TopdownCameraComponent->bUsePawnControlRotation = false;
+	
+	LevelUpNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("LevelUpNiagaraComponent");
+	LevelUpNiagaraComponent->SetupAttachment(GetRootComponent());
+	LevelUpNiagaraComponent->bAutoActivate = false;
+	
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
 	GetCharacterMovement()->bConstrainToPlane = true;
@@ -52,7 +69,19 @@ void AAuraCharacter::AddToXP_Implementation(int32 InXP)
 
 void AAuraCharacter::LevelUp_Implementation()
 {
-	
+	MulticastLevelUpParticles();
+}
+
+void AAuraCharacter::MulticastLevelUpParticles_Implementation() const
+{
+	if (IsValid(LevelUpNiagaraComponent))
+	{
+		const FVector CameraLocation = TopdownCameraComponent->GetComponentLocation();
+		const FVector NiagaraLocation = LevelUpNiagaraComponent->GetComponentLocation();
+		const FRotator ToCameraRotation = (CameraLocation - NiagaraLocation).Rotation();
+		LevelUpNiagaraComponent->SetWorldRotation(ToCameraRotation);
+		LevelUpNiagaraComponent->Activate(true);
+	}
 }
 
 int32 AAuraCharacter::GetXP_Implementation() const
