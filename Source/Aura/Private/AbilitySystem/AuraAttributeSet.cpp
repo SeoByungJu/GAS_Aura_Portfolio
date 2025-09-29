67262897998605ac0d3f1d87db26e7b0961b7ccc
Seobyungju <qudwn0928@gmail.com>
2025-09-29 17:47:18 +0900

    feat(section22): 259강 - 레벨업 인터페이스 및 CombatInterface 리팩터링
    - PlayerInterface에 LevelUp() BlueprintNativeEvent 함수 추가
    - AuraCharacter에서 LevelUp_Implementation() 오버라이드 (레벨업 반응 구조 준비)
    - CombatInterface의 GetPlayerLevel()을 BlueprintNativeEvent로 변경
    - 프로젝트 전역 호출부 Execute_GetPlayerLevel() 방식으로 일관성 확보
    - AuraAttributeSet: IncomingXP 반영 후 레벨업 판별 TODO 추가
    - SendXPEvent() 내 GetPlayerLevel 호출부 Execute 버전으로 교체
    - ExecCalc_Damage: Source/Target PlayerLevel 계산 시 Cast 제거 → Implements<UCombatInterface> + Execute_GetPlayerLevel() 호출
    - AuraAbilitySystemLibrary: Startup Abilities 처리 시 Cast 제거 후 Execute 호출로 수정
    - MMC_MaxMana / MMC_MaxHealth: CombatInterface Cast 제거 → Execute_GetPlayerLevel() 방식 적용

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -177,6 +177,7 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 		SetIncomingXP(0.f);
 
 		// TODO : See if we should Level UP
+		
 		if (Props.SourceCharacter->Implements<UPlayerInterface>())
 		{
 			IPlayerInterface::Execute_AddToXP(Props.SourceCharacter, LocalIncomingXP);
@@ -186,9 +187,9 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 
 void UAuraAttributeSet::SendXPEvent(const FEffectProperties& Props)
 {
-	if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetCharacter))
+	if (Props.TargetCharacter->Implements<UCombatInterface>())
 	{
-		const int32 TargetLevel = CombatInterface->GetPlayerLevel();
+		const int32 TargetLevel = ICombatInterface::Execute_GetPlayerLevel(Props.TargetCharacter);
 		const ECharacterClass TargetClass = ICombatInterface::Execute_GetCharacterClass(Props.TargetCharacter);
 		const int32 XPReward = UAuraAbilitySystemLibrary::GetXPRewardForClassAndLevel(Props.TargetCharacter, TargetClass, TargetLevel);
 
