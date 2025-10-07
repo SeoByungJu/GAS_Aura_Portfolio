7f026ad4ecdd7bccddd07b2d369bfc1ffc52ca0f
Seobyungju <qudwn0928@gmail.com>
2025-10-07 23:02:48 +0900

    feat(section24): 291강 - Spend Spell Point 기능 구현 및 서버–클라이언트 동기화
    - SpellMenuWidgetController
      - SpendPointButtonPressed() BlueprintCallable 함수 추가
      - 현재 선택된 AbilityTag를 AbilitySystemComponent에 전달하도록 구현
    - AuraAbilitySystemComponent
      - ServerSpendSpellPoint() Server RPC 함수 추가
      - AbilitySpec 유효성 검사 및 상태별 처리 로직 분기
        - Eligible → Unlocked: 기존 Eligible 태그 제거 후 Unlocked 태그 추가
        - Unlocked / Equipped: AbilitySpec.Level 증가
      - Spell Point 소모 로직(IPlayerInterface::Execute_AddToSpellPoints(Avatar, -1)) 연동
      - 상태 변경 후 ClientUpdateAbilityStatus(Tag, Status, Level) 호출 (Client RPC)
      - MarkAbilitySpecDirty() 호출로 즉시 복제 반영
      - FAbilityStatusChanged Delegate에 Ability Level 파라미터 추가
    - WB_SpellMenu
      - Spend Point 버튼 클릭 시 SpellMenuWidgetController.SpendPointButtonPressed() 호출
      - 클릭 이벤트 바인딩을 SetWidgetController 이후로 이동 (Null 접근 방지)
    - WB_SpellGlobe_Button
      - 선택된 Spell Globe의 AbilityTag 전달 처리
    - 결과
      - Spell Point 소모 및 Ability 상태(Eligible→Unlocked/LevelUp) 변경 정상 작동
      - 멀티플레이 환경에서도 서버–클라이언트 간 UI 동기화 검증 완료

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -9,7 +9,7 @@
 DECLARE_MULTICAST_DELEGATE_OneParam(FEffectAssetTag, const FGameplayTagContainer& /*AssetTags*/);
 DECLARE_MULTICAST_DELEGATE(FAbilitiesGiven);
 DECLARE_DELEGATE_OneParam(FForEachAbility, const FGameplayAbilitySpec&);
-DECLARE_MULTICAST_DELEGATE_TwoParams(FAbilityStatusChanged, const FGameplayTag& /* AbilityTags*/, const FGameplayTag& /* StatusTags*/);
+DECLARE_MULTICAST_DELEGATE_ThreeParams(FAbilityStatusChanged, const FGameplayTag& /* AbilityTags*/, const FGameplayTag& /* StatusTags*/, int32 /*AbilityLevel*/);
 
 /**
  * 
@@ -45,6 +45,9 @@ public:
 	void ServerUpgradeAttribute(const FGameplayTag& AttributeTag);
 
 	void UpdateAbilityStatuses(int32 Level);
+
+	UFUNCTION(Server, Reliable)
+	void ServerSpendSpellPoint(const FGameplayTag& AbilityTag);
 protected:
 
 	virtual void OnRep_ActivateAbilities() override;
@@ -53,7 +56,7 @@ protected:
 	void ClientEffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
 
 	UFUNCTION(Client, Reliable)
-	void ClientUpdateAbilityStatus(const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag);
+	void ClientUpdateAbilityStatus(const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag, int32 AbilityLevel);
 };
 
 
