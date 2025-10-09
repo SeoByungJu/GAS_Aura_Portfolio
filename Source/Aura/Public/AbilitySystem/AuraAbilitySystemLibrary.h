61c86a55b721a6881e8266e60645b6bef70adfc2
Seobyungju <qudwn0928@gmail.com>
2025-10-09 23:07:39 +0900

    feat(section25): 309강 - Debuff 데이터 EffectContext 전달 및 AttributeSet 연동
    - ExecCalc_Damage의 DetermineDebuff() 이후 AuraEffectContext에 Debuff 데이터 기록 구조 추가
    - AuraAbilitySystemLibrary에 Setter 함수 5종 추가:
      → SetIsSuccessfulDebuff(), SetDebuffDamage(), SetDebuffDuration(), SetDebuffFrequency(), SetDamageType()
    - SetDamageType()은 GameplayTag SharedPointer 기반으로 구현 (MakeShared 사용)
    - ExecCalc_Damage에서 DebuffDamage, Duration, Frequency 값을 SpecHandle.GetSetByCallerMagnitude()로 추출 후 Context에 전달
    - AuraEffectContext에 SetDamageType(const TSharedPtr<FGameplayTag>) 함수 추가
    - AuraAttributeSet 리팩토링:
      → HandleIncomingDamage(), HandleIncomingXP() 함수 분리 (PostGameplayEffectExecute 단순화)
      → HandleIncomingDamage() 내 Debuff 처리 분기 추가 (IsSuccessfulDebuff() 기반 HandleDebuff() 호출)
      → HandleDebuff() 함수 신설, Debuff 처리 로직 전용 구조 구축

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -73,6 +73,20 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
 	static void SetIsCriticalHit(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, bool bInIsCriticalHit);
 
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetIsSuccessfulDebuff(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, bool bInSuccessfulDebuff);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffDamage(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InDamage);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffDuration(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InDuration);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void SetDebuffFrequency(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, float InFrequency);
+
+	static void SetDamageType(UPARAM(ref) FGameplayEffectContextHandle& EffectContextHandle, const FGameplayTag& InDamageType);
+
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayMechanics")
 	static void GetLivePlayerWithinRadius(const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, const TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
 
@@ -84,3 +98,4 @@ public:
 
 	static int32 GetXPRewardForClassAndLevel(const UObject* WorldContextObject, ECharacterClass CharacterClass, int32 CharacterLevel);
 };
+
